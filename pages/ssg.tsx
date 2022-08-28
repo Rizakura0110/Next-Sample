95251bab4506c32bb01e1319007bd84aba94c51e
Rizakura0110 <rasakura0110@gmail.com>
2022-08-28 23:41:13 +0900

    getStaticProps

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,9 +1,13 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage, NextPageContext } from "next";
 import Head from "next/head";
 
-type SSGProps = {}
+type SSGProps = {
+    message: string
+}
 
-const SSG: NextPage<SSGProps> = () => {
+const SSG: NextPage<SSGProps> = (props) => {
+    const { message } = props
+
     return(
         <div>
             <Head>
@@ -14,9 +18,23 @@ const SSG: NextPage<SSGProps> = () => {
                 <p>
                     静的サイト生成
                 </p>
+                <p>
+                    { message }
+                </p>
             </main>
         </div>
     )
+}
+
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+    const timestamp = new Date().toLocaleString()
+    const message = `${ timestamp } に getStaticProps が 実行 さ れ まし た `
+    console.log(message)
+    return {
+        props: {
+            message,
+        }
+    }
 }
 
 export default SSG
